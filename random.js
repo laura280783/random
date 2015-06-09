38acce3c9d30bbeda5e7ff24e0c9787f7510c003
Laura Gabriela Islas Alcantara <islaslga@gmail.com>
2015-06-09 09:46:47 -0500

    Segundo cambio
    Sumarización de proceso.

--- random.js
+++ random.js
@@ -2,10 +2,12 @@
 
 var numero = Math.random();
 
+var str = ' MAYOR que 0.5 \n';
+
 if(numero<=0.5)
 {
-  console.log('\n' + numero + ' MENOR que 0.5 \n');
+  str= ' MENOR que 0.5 \n';
 }
-else{
-  console.log('\n' + numero + ' MAYOR que 0.5 \n');
-}
+
+
+console.log('\n' + numero + str + ' n');
